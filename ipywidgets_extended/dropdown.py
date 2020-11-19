31dbc1a951e37679f4111994b3f971303bdd20ac
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-11-19 02:13:02 +0100

    Ensure starting value for dropdown + example NB
    Add example notebook that shows off `DropdownExtended`.

--- ipywidgets_extended/dropdown.py
+++ ipywidgets_extended/dropdown.py
@@ -1,7 +1,7 @@
 """Dropdown Widget Extension"""
 from typing import List
 
-from ipywidgets.widgets.widget_selection import Dropdown
+from ipywidgets.widgets.widget_selection import Dropdown, _make_options
 from traitlets import traitlets
 
 from ipywidgets_extended.version import __version__
@@ -26,6 +26,28 @@ class DropdownExtended(Dropdown):
     _view_module_version = traitlets.Unicode(__version__).tag(sync=True)
 
     disabled_options = traitlets.List([]).tag(sync=True)
+
+    def __init__(self, *args, **kwargs):
+        options = _make_options(kwargs.get("options", ()))
+
+        # Ensure initialized 'index' is an enabled option (if possible)
+        if (
+            "index" not in kwargs
+            and "value" not in kwargs
+            and "label" not in kwargs
+            and "disabled_options" in kwargs
+        ):
+            for index, option in enumerate(options):
+                if option[0] not in kwargs["disabled_options"]:
+                    kwargs["index"] = index
+                    kwargs["label"], kwargs["value"] = options[index]
+                    break
+            else:
+                if options:
+                    kwargs["index"] = kwargs["label"] = kwargs["value"] = None
+
+        super().__init__(*args, **kwargs)
+        self._initializing_traits_ = False
 
     @traitlets.validate("disabled_options")
     def _validate_disabled_options(self, proposal: dict) -> List[str]:
