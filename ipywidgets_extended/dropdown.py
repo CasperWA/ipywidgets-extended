6f55bf79056c2ad3eeab27d1bee863a969629eeb
Casper Welzel Andersen <casper.andersen@epfl.ch>
2021-03-29 13:43:12 +0200

    Add grouping option
    The new `grouping` can be set to create bold headers and slightly
    indented values within in the Dropdown.
    The headers cannot be chosen - they are disabled.

--- ipywidgets_extended/dropdown.py
+++ ipywidgets_extended/dropdown.py
@@ -1,5 +1,5 @@
 """Dropdown Widget Extension"""
-from typing import List
+from typing import List, Tuple
 
 from ipywidgets.widgets.widget_selection import Dropdown, _make_options
 from traitlets import traitlets
@@ -15,6 +15,7 @@ class DropdownExtended(Dropdown):
 
     Extensions:
     - Disable individual options.
+    - Create groupings within the dropdown.
 
     """
 
@@ -25,14 +26,30 @@ class DropdownExtended(Dropdown):
     _view_module = traitlets.Unicode("ipywidgets-extended").tag(sync=True)
     _view_module_version = traitlets.Unicode(f"^{__version__}").tag(sync=True)
 
-    # This being read-only means that it cannot be changed by the user.
+    # Additional widget properties used in the TS
     _disabled_options_labels = traitlets.List(
         trait=traitlets.Unicode(),
         read_only=True,
         help="The labels for the disabled options.",
     ).tag(sync=True)
+    _grouping_labels = traitlets.List(
+        traitlets.Tuple(traitlets.Unicode(), traitlets.List(traitlets.Unicode())),
+        read_only=True,
+        help=(
+            "List of tuples for groupings, where the first value in the tuple is the grouping "
+            "header and the second value is a list of the labels in the grouping."
+        ),
+    ).tag(sync=True)
 
-    disabled_options = traitlets.List([]).tag(sync=True)
+    # The equivalent Python changeable traits
+    disabled_options = traitlets.List(traitlets.Unicode(), default_value=[])
+    grouping = traitlets.List(
+        traitlets.Tuple(
+            traitlets.Unicode(),
+            traitlets.List(traitlets.Unicode()),
+        ),
+        default_value=[],
+    )
 
     def __init__(self, *args, **kwargs):
         self._initializing_traits_ = True
@@ -77,6 +94,9 @@ class DropdownExtended(Dropdown):
         self.set_trait("_disabled_options_labels", disabled_options)
         if not self._initializing_traits_:
             if disabled_options:
+                grouping_entries = range(
+                    sum(len(_) + 1 for _ in dict(self._grouping_labels).values())
+                )
                 if self._options_labels[self.index] in disabled_options:
                     for index, label in enumerate(self._options_labels):
                         if label not in disabled_options:
@@ -84,10 +104,49 @@ class DropdownExtended(Dropdown):
                             break
                     else:
                         self.index = None
-            elif self._options_labels:
+                elif grouping_entries[self.index] in disabled_options:
+                    for index, label in enumerate(grouping_entries):
+                        if (
+                            label not in disabled_options
+                            and label not in self._group_headers
+                        ):
+                            self.index = index
+                            break
+                    else:
+                        self.index = None
+            elif self._options_labels and not self._grouping_labels:
                 if self.index == 0:
                     self._notify_trait("index", 0, 0)
                 else:
                     self.index = 0
             else:
                 self.index = None
+
+    @traitlets.validate("grouping")
+    def _validate_grouping(self, proposal) -> List[Tuple[str, List[str]]]:
+        """Ensure all group headers are unique"""
+        if proposal.value is None or not proposal.value:
+            return []
+        assert len(proposal.value) == len(
+            dict(proposal.value).keys()
+        ), f"Group headers must be unique. Passed group headers: {[_[0] for _ in proposal.value]}"
+        return proposal.value
+
+    @traitlets.observe("grouping")
+    def _set_grouping(self, change) -> None:
+        """Put options into desired grouping, updating `options`"""
+        grouping = change.new
+        self.set_trait("_grouping_labels", grouping)
+        if not self._initializing_traits_:
+            if not grouping and self._options_labels:
+                if self.index == 0:
+                    self._notify_trait("index", 0, 0)
+                else:
+                    self.index = 0
+            else:
+                self.index = None
+
+    @property
+    def _group_headers(self) -> List[str]:
+        """Get group headers from self._grouping_labels"""
+        return [_[0] for _ in self._grouping_labels]
